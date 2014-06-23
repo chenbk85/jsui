caef91a842376f71d682bb49e50d87bc63e53d81
tony <kks@192.168.0.102>
2014-06-24 01:26:29 +0800

    updated

--- jitems.js
+++ jitems.js
@@ -1,6 +1,6 @@
 // Item.
 
-jsui.resumeItem = function(_name, _jform, _icon, _desc, _txt, _link) {
+jsui.resumeItem = function(_name, _jform, _icon, _desc, _txt0, _link0, _txt1, _link1) {
     var jthis = jclass("resumeItem", "control", _name, _jform);
 
     jthis.panel = jsui.rect(undefined, _jform)
@@ -28,18 +28,33 @@ jsui.resumeItem = function(_name, _jform, _icon, _desc, _txt, _link) {
     ;
     jthis.panel.add(jthis.desc);
 
-    jthis.link = jsui.url(undefined, j)
-        .setText(_txt)
-        .setUrl(_link)
+    jthis.link0 = jsui.url(undefined, j)
+        .setText(_txt0)
+        .setUrl(_link0)
     ;
-    jthis.panel.add(jthis.link);
+    jthis.panel.add(jthis.link0);
+
+    if (_txt1 != undefined) {
+        jthis.link1 = jsui.url(undefined, j)
+            .setText(_txt1)
+            .setUrl(_link1)
+        ;
+        jthis.panel.add(jthis.link1);
+    }
 
     jthis.override("setSize", function(w, h) {
             jthis.baseObj.setSize(w, h);
             jthis.panel.setSize(w, h);
-            jthis.link.setPosition(w - 40, 40);
-            var s = jthis.link.measure();
-            jthis.link.setSize(s.width, -jthis.link.fontSize);
+
+            jthis.link0.setPosition(w - 60, 40);
+            var s = jthis.link0.measure();
+            jthis.link0.setSize(s.width, -jthis.link0.fontSize);
+
+            if (_txt1 != undefined) {
+                jthis.link1.setPosition(w - 120, 40);
+                var _s = jthis.link1.measure();
+                jthis.link1.setSize(_s.width, -jthis.link1.fontSize);
+            }
 
             return jthis;
         }
